861b202add602abcf7cb4d22bd5e2a5cffd3ccc0
shanetoken <pphu@business.fb.com>
2024-04-11 14:04:34 +0800

    Router

--- src/ui/Sidebar/Sidebar.jsx
+++ src/ui/Sidebar/Sidebar.jsx
@@ -1,13 +1,28 @@
 import Logo from "./Logo";
 import SidebarMenu from "./SidebarMenu";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "/pages/Dashboard";
+import Bookings from "/pages/Bookings"
+import Cabins from "/pages/Cabins"
+import Users from "/pages/Users"
+import Settings from "/pages/Users"
 
-const borderBox = "border-[1px] border-gray-100" 
+const borderBox = "border-[1px] border-gray-100";
 
 function Sidebar() {
   return (
-    <div className={`${borderBox} w-60 text-center flex flex-col font-semibold items-center pt-8`}>
+    <div
+      className={`${borderBox} w-60 text-center flex flex-col font-semibold items-center pt-6`}
+    >
       <Logo />
       <SidebarMenu />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/cabins" element={<Cabins />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/setting" element={<Settings />} />
+      </Routes>
     </div>
   );
 }
--- src/ui/Sidebar/SidebarMenu.jsx
+++ src/ui/Sidebar/SidebarMenu.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 function SidebarMenu() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav>
+      {/* Replace with your link names and paths */}
+      <Link to="/dashboard">🏠 Home</Link>
+      <Link to="/bookings">📆 Booking</Link>
+      <Link to="/cabins">🏕️ Cabins</Link>
+      <Link to="/users">👥 Users</Link>
+      <Link to="/users">⚙️ Settings</Link>
+    </nav>
+  );
 }
 
-export default SidebarMenu
+export default SidebarMenu;
