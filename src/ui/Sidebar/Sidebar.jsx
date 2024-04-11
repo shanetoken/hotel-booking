597f330d4a4968a10cc6393b761dc8dca02fac25
shanetoken <pphu@business.fb.com>
2024-04-11 14:27:29 +0800

    Edit router

--- src/ui/Sidebar/Sidebar.jsx
+++ src/ui/Sidebar/Sidebar.jsx
@@ -1,11 +1,5 @@
 import Logo from "./Logo";
 import SidebarMenu from "./SidebarMenu";
-import { Route, Routes } from "react-router-dom";
-import Dashboard from "/pages/Dashboard";
-import Bookings from "/pages/Bookings"
-import Cabins from "/pages/Cabins"
-import Users from "/pages/Users"
-import Settings from "/pages/Users"
 
 const borderBox = "border-[1px] border-gray-100";
 
@@ -16,13 +10,6 @@ function Sidebar() {
     >
       <Logo />
       <SidebarMenu />
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/bookings" element={<Bookings />} />
-        <Route path="/cabins" element={<Cabins />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/setting" element={<Settings />} />
-      </Routes>
     </div>
   );
 }
--- src/ui/Sidebar/SidebarMenu.jsx
+++ src/ui/Sidebar/SidebarMenu.jsx
@@ -1,15 +1,29 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "/home/shanetoken/hotel-booking/src/pages/Dashboard";
+// import Bookings from "Bookings";
+// import Cabins from "Cabins";
+// import Users from "Users";
+// import Settings from "Users";
 
 function SidebarMenu() {
   return (
-    <nav>
-      {/* Replace with your link names and paths */}
-      <Link to="/dashboard">🏠 Home</Link>
-      <Link to="/bookings">📆 Booking</Link>
-      <Link to="/cabins">🏕️ Cabins</Link>
-      <Link to="/users">👥 Users</Link>
-      <Link to="/users">⚙️ Settings</Link>
-    </nav>
+    <>
+      <nav>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        {/* <NavLink to="/bookings">Bookings</NavLink>
+        <NavLink to="/cabins">Cabins</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/settings">Settings</NavLink> */}
+      </nav>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/bookings" element={<Bookings />} />
+        <Route path="/cabins" element={<Cabins />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/settings" element={<Settings />} /> */}
+      </Routes>
+    </>
   );
 }
 
